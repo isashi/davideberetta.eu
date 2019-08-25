d217a1368edf50716d577c93c3e04ef78b39ba6f
Isashi <isashimitsui@gmail.com>
2019-08-26 00:06:19 +0200

    header WIP

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <div class="hero-text-box">
+      <h1>This is my header</h1>
+      <a href="#">click me</a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,18 @@
 <template>
-  <section>
-    <div>
-      <Logo :width="350"/>
-      <h1 class="title">
-        <span>davideberetta.eu</span>
-      </h1>
-    </div>
-  </section>
+  <div>
+    <Header></Header>
+  </div>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-import IconLink from "~/components/IconLink.vue";
+import Header from "@/components/Header.vue";
 
 export default {
   components: {
-    Logo,
-    IconLink
+    Header
   }
 };
 </script>
 
 <style scoped>
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 400;
-  font-size: 100px;
-  color: #2E495E;
-  letter-spacing: 1px;
-  font-size: 6em;
-}
-.green {
-  color: #00C48D;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 3em;
-  color: #2E495E;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
